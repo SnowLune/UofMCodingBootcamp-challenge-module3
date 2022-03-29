269aa588f6bf63cc4934fd6a483373c5dfa3b468
Luna R <snowluna@protonmail.com>
2022-03-29 18:59:06 +0000

    flesh out js

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var special = " !" + "#$%&" + "()*+,-./:;<=>?@[]^_`{|}~";
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var c_Special = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+var c_Lower = "abcdefghijklmnopqrstuvwxyz";
+var c_Upper = c_Lower.toUpperCase();
+var c_Number = "0123456789";
+var c_All = c_Special + c_Lower + c_Upper + c_Number;
 
-function generatePassword()
+function generatePassword(passwordLength = 16, b_Alpha = true, b_Numeric = true, b_Special = true)
 {
-   
+   if (passwordLength <= 0)
+   {
+      console.error("How did this even happen?");
+   }
+
+   // Set characters we can use
+   chars = c_Upper + c_Lower;
+   if (b_Numeric === true)
+   {
+      chars = chars + c_Number;
+   }
+   if (b_Special === true)
+   {
+      chars = chars + c_Special;
+   }
+
+   // Actually generate the password
+
+   var newPassword = "";
+
+   for (let i = 0; i < passwordLength; i++)
+   {
+      newPassword = newPassword 
+            + chars[Math.floor(Math.random() * chars.length)]
+   }
+
+   return newPassword;
 }
 
 // Write password to the #password input
-function writePassword() {
+function writePassword()
+{
    var password = generatePassword();
    var passwordText = document.querySelector("#password");
 
